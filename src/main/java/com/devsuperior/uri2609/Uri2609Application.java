a51cf2b36d44ebfaa2260b59723645010aea1f13
JoaoPaulo <jpaulofsantos@gmail.com>
2024-01-07 14:45:07 -0300

    SQL query (dto and projection)

--- src/main/java/com/devsuperior/uri2609/Uri2609Application.java
+++ src/main/java/com/devsuperior/uri2609/Uri2609Application.java
@@ -1,11 +1,16 @@
 package com.devsuperior.uri2609;
 
+import com.devsuperior.uri2609.dto.CategorySumDTO;
+import com.devsuperior.uri2609.projections.CategorySumProjection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.devsuperior.uri2609.repositories.CategoryRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class Uri2609Application implements CommandLineRunner {
@@ -19,6 +24,23 @@ public class Uri2609Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+
+		List<CategorySumProjection> categorySumProjectionList = repository.searchSQL();
+		List<CategorySumDTO> categorySumDTOList = categorySumProjectionList.stream().map(x-> new CategorySumDTO(x)).collect(Collectors.toList());
+
+		System.out.println("");
+		System.out.println("-----SQL-PROJ-----");
+
+		for (CategorySumProjection item: categorySumProjectionList) {
+			System.out.println(item.getName() + " - " + item.getSum());
+		}
+
+		System.out.println("");
+		System.out.println("-----SQL-DTO-----");
+
+		for (CategorySumDTO item: categorySumDTOList) {
+			System.out.println(item.getName() + " - " + item.getSum());
+		}
 
 	}
 }
